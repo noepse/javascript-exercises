4d520a8f3058b68d66edc341aeae9bd0b261026e
simmi <22905246+noepse@users.noreply.github.com>
2023-11-10 11:01:16 +0000

    Add exercise answer

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const letters = str.replaceAll(/[^A-z0-9]/g, "").split("");
+    const reversedLetters = [];
 
+    for (let i = letters.length - 1; i>=0; i--){
+        reversedLetters.push(letters[i]);
+    }
+    if (reversedLetters.join('').toLowerCase() === letters.join('').toLowerCase()){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
