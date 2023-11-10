dd5747cb3e6b5d4c6351810a8c266c12ef19a1d4
simmi <22905246+noepse@users.noreply.github.com>
2023-11-10 12:45:26 +0000

    Add exercise answer

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((currentOldest, currentPerson) => {
+        const currentOldestAge = calculateAge(currentOldest);
+        const currentPersonAge = calculateAge(currentPerson);
 
-};
+        if (currentPersonAge > currentOldestAge){
+            currentOldest = currentPerson
+        }
+        return currentOldest;
+    })
+
+
+    function calculateAge(person){
+        if (person.yearOfDeath){
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        else{
+            const date = new Date();
+            person.age = date.getFullYear() - person.yearOfBirth;
+        }
+        return person.age
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
