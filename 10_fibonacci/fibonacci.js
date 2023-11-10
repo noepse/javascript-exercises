e6f307cc2b8c3ef08853e76f4ac6ffb1c195bb6f
simmi <22905246+noepse@users.noreply.github.com>
2023-11-10 11:18:55 +0000

    Add exercise answer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<=0){
+        return 'OOPS'
+    }
+    const indexToFind = parseInt(num) - 1;
+    const sequence = [1, 1];
+    if (indexToFind === 0 || indexToFind === 1){
+        return sequence[indexToFind]
+    }
+    if (indexToFind>1){
+        for (i = 2;i <=indexToFind ; i++){
+            sequence[i] = sequence[i-1] + sequence[i-2];
+        }
+        return sequence[indexToFind];
+    }
 };
 
 // Do not edit below this line
